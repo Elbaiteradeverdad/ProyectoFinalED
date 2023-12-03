91c6fda702a28de21548fe5b936597bfaeb9d54b
Shonen <Shonen@LAPTOP-L11JAVDE>
2023-12-03 17:37:33 -0500

    Probando

--- src/proyectofinaled/ProyectoFinalED.java
+++ src/proyectofinaled/ProyectoFinalED.java
@@ -6,7 +6,9 @@ public class ProyectoFinalED {
 
     public static void main(String[] args) {
        Empresa empresa=new Empresa();
-       
+       Pais aux=(Pais)empresa.getPaises().getPrimero().getDato();
+       Sucursal aux2=(Sucursal)aux.getSucursales().getPrimero().getDato();
+        System.out.println(aux2.getTrabajadores().Mostrar());
     }
     
 }
